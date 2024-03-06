95587e509390609c80a5d4064395ee15e71ffcc7
Luna <tkdgks7036@naver.com>
2024-03-06 18:06:21 +0900

    fix: dto 에 validator decorator 사용

--- src/api/todos/dto/create-todo.dto.ts
+++ src/api/todos/dto/create-todo.dto.ts
@@ -1,4 +1,19 @@
+import { IsBoolean, IsString, MaxLength, MinLength } from "class-validator";
+
 export class CreateTodoDto {
+  @IsString({
+    message: `Title type must be string.`
+  })
+  @MinLength(5, {
+    message: `Title must be at least 5 characters long.`
+  })
+  @MaxLength(10, {
+    message: `Title can be up to 30 characters long.`
+  })
   title: string;
+
+  @IsBoolean({
+    message: `Writing must be of Boolean type.`
+  })
   is_doen: boolean;
 }
